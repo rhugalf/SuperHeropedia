182f8697dd63a0c2db80d02276989e3fce694c42
A webo putos <glb-mxm0004@MacBook-Pro-de-GLB-MXM0004.local>
2015-07-02 00:35:34 -0500

    first commit

---
+++ SuperHeropedia/Superhero.h
@@ -0,0 +1,13 @@
+//
+//  Superhero.h
+//  SuperHeropedia
+//
+//  Created by Hugo on 10/20/14.
+//  Copyright (c) 2014 Hugo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Superhero : NSObject
+
+@end
---
+++ SuperHeropedia/Superhero.m
@@ -0,0 +1,13 @@
+//
+//  Superhero.m
+//  SuperHeropedia
+//
+//  Created by Hugo on 10/20/14.
+//  Copyright (c) 2014 Hugo. All rights reserved.
+//
+
+#import "Superhero.h"
+
+@implementation Superhero
+
+@end
